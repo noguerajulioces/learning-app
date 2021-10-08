cf9cce07d859609282f8d9bab5659e7c0e0b7e7f
Julio Noguera <noguerajulio24@gmail.com>
2021-10-08 16:42:39 -0500

    add trackable

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :courses
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'static_pages/landing_page'
   get 'static_pages/privacy_policy'
