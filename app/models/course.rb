4d64de7a21d98f40c08afcaec835dd125dc2d841
Julio Noguera <noguerajulio24@gmail.com>
2021-10-08 12:53:56 -0500

    add facker

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
     validates :title, presence: true
-    validates :description, presence: true, length: {minimum:10, maximum:100}
+    validates :description, presence: true, length: {minimum:10}
    
     def to_s
         title
